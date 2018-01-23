fb7a643e6dcd32006d8caabc57b1451a73dd55be
pengqin <peng.qin@fiserv.com>
2018-01-24 11:13:53 +1300

    Add mysql setup in setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+db_user=supersaver
+db_userpwd=supersaver123
 
 # Prepare environment for Scrapy on Mac OSX
 xcode-select --install
@@ -8,8 +10,16 @@ brew install libtiff libjpeg webp little-cms2
 
 # * Install mysql
 brew install mysql
+# Install 
+curl -O https://cdn.mysql.com//Downloads/Connector-Python/mysql-connector-python-2.1.7-osx10.12.dmg
 # * Install nginx (linux)
 
 pip install -r requirements.txt
 
 ./patches/apply_patches.sh
+
+
+mysql -uroot -p<<EOF
+CREATE USER '${db_user}'@'localhost' IDENTIFIED BY '${db_userpwd}';
+flush PRIVILEGES;
+EOF

d30d5f429b1f8402f5e8c7a2fb972e27c2ae6f51
pengqin <peng.qin@fiserv.com>
2018-01-23 17:48:14 +1300

    Add setup screen to install Selenium and Chrome Driver.

---
+++ setup_selenium.sh
@@ -0,0 +1,5 @@
+curl -O http://selenium-release.storage.googleapis.com/3.8/selenium-server-standalone-3.8.1.jar
+curl -O https://chromedriver.storage.googleapis.com/2.35/chromedriver_mac64.zip
+unzip chromedriver_mac64.zip
+mv chromedriver ./bin
+mv selenium-server-standalone-3.8.1.jar ./bin

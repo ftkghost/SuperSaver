a039f6205e6699b1f90cb7cc0fa397fcbcf30b2d
pengqin <ftkghost@gmail.com>
2018-01-28 01:23:12 +1300

    Update lasoo.co.nz spider with basic crawling structure.

--- crawler/dealcrawler/spiders/nz/LasooCoNzDealSpider.py
+++ crawler/dealcrawler/spiders/nz/LasooCoNzDealSpider.py
@@ -10,11 +10,14 @@ import random
 import scrapy
 from supersaver.settings import make_internal_property_name
 
-from retailer.models import Retailer
+from dealcrawler.spiders.BaseSpider import BaseSpider
+from retailer.models import Retailer, RetailerProperty
 from category.models import Category
 from store.models import Store
 from region.models import Region
 from product.models import Product, ProductImage
+from supersaver.constants import *
+from product.models import ProductProperty
 
 from dealcrawler.util import *
 from dealcrawler.model.items import ProductItem, StoreItem, RegionItem
@@ -23,10 +26,10 @@ from dealcrawler.model.items import ProductItem, StoreItem, RegionItem
 UTC_TO_NZ_TIMEZONE_DELTA = timedelta(seconds=12*3600)
 
 
-class LasooCoNzDealSpider(scrapy.Spider):
+class LasooCoNzDealSpider(BaseSpider):
 
     """
-    Crawl deals from lasso.co.nz
+    Crawl deals from lasoo.co.nz
     """
     name = 'lasoo.co.nz'
     user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -36,22 +39,47 @@ class LasooCoNzDealSpider(scrapy.Spider):
 
     }
 
-    SVER = 'ikt3z7fsszp9rt201651'
+    # SVER = 'ikt3z7fsszp9rt201651'
     ORIGIN_URL = 'https://www.lasoo.co.nz/'
 
-    JSONP_HOST = 'www.lasoo.co.nz'
-    RETAILER_INFO_URL_FORMAT = 'https://www.lasoo.co.nz/data.js?type=catalogue' \
+    REQUEST_HOST = 'www.lasoo.co.nz'
+    CATALOGUE_INFO_URL_FORMAT = 'https://www.lasoo.co.nz/data.js?type=catalogue' \
                                '&catalogueids={0}&jsonp={1}'
     RETAILER_DEALS_URL_FORMAT = 'https://www.lasoo.co.nz/data.js?type=cataloguepage' \
                                 '&catalogueid={0}&allpages=1&jsonp={1}'
     OFFER_DETAILS_URL_FORMAT = 'https://www.lasoo.co.nz/data.js?type=offer&id={0}&jsonp={1}'
 
-    def __init__(self):
-        random.seed(datetime.now().timestamp())
-        pass
+    RETAILER_LIST_URL_REFERER = 'https://www.lasoo.co.nz/retailers.html'
+    # 1. Filter parameter is single capital character 'ABCD...XYZ' or 0(zero)
+    # 2. xpath:  //div[@class="ajax_loader"]//ul[contains(@class, "list")/li
+#     <li>
+#     <span id="13522443117335" class="icon_favourite cb_popupAlert" onclick="submitAddFavouriteRetailer2(this, 13522443117335);return false;"></span>
+#
+#     <a id="retailerid_13522443117335" title="All Power" href="/retailer/all-power.html" class="onclick_href_tracking" data="{&quot;placement&quot;:&quot;retailers&quot;,&quot;object&quot;:&quot;retailer&quot;,&quot;objectid&quot;:&quot;13522443117335&quot;,&quot;interaction&quot;:&quot;RetailerSelectRequest&quot;}">All Power
+#     </a>
+#     <div class="tooltip">
+#     <div class="tooltip_inner">Add to <br> My Favourites</div>
+# </div>
+# </li>
+    # 3. Retailer website link and logo may be found from retailer landing page:
+    #    https://www.lasoo.co.nz/retailer/{retailer_name}.html
+    # 4. Get store location (Some retailer may not have store)
+    #    https://www.lasoo.co.nz/storelocator/{retailer_name}/location/{region}.html
+    # 5. Parse store name, id, address, open hours
 
-    def start_requests(self):
-        yield scrapy.Request(self.__class__.ORIGIN_URL, callback=self.parse)
+    RETAILER_LIST_URL_FORMAT = 'https://www.lasoo.co.nz/retailers.html?filter={0}&requestType=ajax'
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            DATASOURCE_ID_LASOO_CO_NZ,
+            'NZ',
+            self.__class__.ORIGIN_URL,
+            self.__class__.REQUEST_HOST,
+            *args, **kwargs)
+        random.seed(datetime.now().timestamp())
+
+    # TODO: Parse retailer list and store list first with higher request priority
 
     def parse(self, response):
         for cat_elem in response.xpath('//ul[contains(@class, "catalogue-list")]/li/div/a'):
@@ -61,22 +89,161 @@ class LasooCoNzDealSpider(scrapy.Spider):
             if data['object'] == 'catalogue':
                 catalogue_id = data['objectid']
                 jsonp_tag = self.__class__._get_random_jsonp_tag()
-                reatail_detail_url = self.__class__._get_retailer_detail_url(catalogue_id, jsonp_tag)
-                yield scrapy.Request(reatail_detail_url,
-                                     callback=self.parse_retailer_details,
-                                     headers={
-                                         'Accept': '*/*',
-                                         'Accept-Language': 'en-US,en;q=0.8',
-                                         'Host': self.__class__.JSONP_HOST,
-                                         'Referer': cat_detail_url,
-                                     },
+                retailer_detail_url = self.__class__._get_retailer_detail_url(catalogue_id, jsonp_tag)
+                yield scrapy.Request(retailer_detail_url,
+                                     callback=self.parse_retailer_from_response,
+                                     headers=self.__class__._get_http_headers(cat_detail_url),
                                      meta={
                                          'catalogue_id': catalogue_id,
                                          'referer': cat_detail_url,
                                          'jsonp_tag': jsonp_tag,
                                      })
+                break  # To remove
             else:
                 self.log("unknown catalogue object ({0}). {1}", cat_detail_url, data)
+
+    def parse_retailer_from_response(self, response):
+        json_data = self.__class__._get_jsonp_response_data(response.body, response.meta['jsonp_tag'])
+        catalogues = json_data['catalogues']
+        if len(catalogues) == 0:
+            return []
+        catalogue = catalogues[0]
+        start_date = dateparser.parse(catalogue['startDate']).timestamp()
+        end_date = dateparser.parse(catalogue['expiryDate']).timestamp()
+        total_pages = catalogue['numberPages']
+        retailer_data = catalogue['retailer']
+        retailer = self._get_retailer_from_json(retailer_data)
+
+        # Update jsonp for new request
+        jsonp_tag = self.__class__._get_random_jsonp_tag()
+        deal_url = self.__class__._get_deals_url(response.meta['catalogue_id'], jsonp_tag)
+        meta = response.meta
+        meta['offer_start_date'] = start_date
+        meta['offer_end_date'] = end_date
+        meta['retailer'] = retailer
+        meta['jsonp_tag'] = jsonp_tag
+        yield scrapy.Request(deal_url,
+                             callback=self.parse_deals_from_response,
+                             headers=self.__class__._get_http_headers(meta['referer']),
+                             meta=meta)
+
+    def _get_retailer_from_json(self, retailer_json):
+        properties = []
+        prop = RetailerProperty()
+        prop.name = make_internal_property_name('lasoo_id')
+        prop.value = retailer_json['id']
+        properties.append(prop)
+
+        prop = RetailerProperty()
+        prop.name = make_internal_property_name('lasoo_unique_name')
+        prop.value = retailer_json['uniqueName']
+        properties.append(prop)
+
+        prop = RetailerProperty()
+        prop.name = make_internal_property_name('lasoo_code')
+        prop.value = retailer_json['code']
+        properties.append(prop)
+
+        return self._create_or_update_retailer_in_db(retailer_json['name'], retailer_json['smallImage'], properties)
+
+    def parse_deals_from_response(self, response):
+        retailer = response.meta['retailer']
+        offer_start_time = response.meta['offer_start_date']
+        offer_end_time = response.meta['offer_end_date']
+
+        deals_data = self._get_jsonp_response_data(response.body, response.meta['jsonp_tag'])
+        # if 'categoryPath' in offer:
+        #     cats = offer['categoryPath']
+        #     for c in cats:
+        #         category = SourceCategory()
+        #         c.name = c['uniqueName']
+        #         c.source_category_id = c['id']
+        #         c.display_name = c['name']
+        #         c.source = self.datasource
+        #         c.save()
+
+        for page in deals_data:
+            offers = page['offers']
+            if offers is None:
+                continue
+            for offer_data in offers:
+                if offer_data['type'] != 'offer':
+                    continue
+                offer = ProductItem()
+                offer['retailer'] = retailer
+                offer['title'] = offer_data['title']
+                offer['promotion_start_date'] = offer_start_time
+                offer['promotion_end_date'] = offer_end_time
+                offer['landing_page'] = response.urljoin(offer_data['landingLink'])
+
+                meta = {
+                    'offer': offer,
+                    'referer': response.url,
+                }
+                if 'offerimage' in offer_data:
+                    meta['offer_image'] = offer_data['offerimage']['path']
+                # Update jsonp for new request
+                jsonp_tag = self.__class__._get_random_jsonp_tag()
+                meta['jsonp_tag'] = jsonp_tag
+                offer_json_url = self.__class__._get_offer_url(offer_data['id'], jsonp_tag)
+                yield scrapy.Request(offer_json_url,
+                                     callback=self.parse_offer_details_from_response,
+                                     headers=self.__class__._get_http_headers(response.url),
+                                     meta=meta)
+
+    def parse_offer_details_from_response(self, response):
+        # Parse Json
+        data = self._get_jsonp_response_data(response.body, response.meta['jsonp_tag'])
+        if len(data) == 0:
+            self.log('Failed to get offer details {0}'.format(response.url), level=logging.WARN)
+            return
+        offer_data = data[0]
+        offer = response.meta['offer']
+        offer['price'] = offer_data['priceValue']
+        offer['saved'] = None if offer_data['saving'] is None or len(offer_data['saving']) else offer_data['saving']
+
+        prop = ProductProperty()
+        prop.name = make_internal_property_name('lasoo_id')
+        prop.value = offer_data['id']
+
+        meta = {
+            'offer': offer,
+            'offer_property': prop,
+        }
+        if 'offer_image' in response.meta:
+            meta['offer_image'] = response.meta['offer_image']
+        return scrapy.Request(offer['landing_page'],
+                              callback=self.parse_offer_stores_response,
+                              headers=self.__class__._get_http_headers(meta['referer']),
+                              meta=meta)
+
+    def parse_offer_stores_response(self, response):
+        # Parse store
+        script = extract_first_value_with_xpath(
+            '//section//div[@class="storemap-holder"]/following-sibling::script[1]/text()')
+
+        idx = script.find('showOfferDetailNearStoreMap')
+        if idx < 0:
+            return None
+        locations_json = substr_surrounded_by_chars(script, ('[', ']'), idx)
+        store_locations = json_loads(locations_json)
+        offer = response.meta['offer']
+        for l in store_locations:
+            s = Store()
+            s.retailer = offer.retailer
+            name = l['displayName'].replace(' -- ', ' - ')
+            s.name = name
+            s.display_name = name
+            s.latitude = l['latitude']
+            s.longitude = l['longitude']
+
+        # [
+        #     {id:14540226467431,latitude:-36.9424592,longitude:174.7867304,displayName:"FreshChoice -- Mangere Bridge'"}
+        #     ,
+        #     {id:14776308161975,latitude:-36.9446156,longitude:174.8425279,displayName:"FreshChoice -- Otahuhu'"}
+        #     ,
+        #     {id:14540226467325,latitude:-36.8809,longitude:174.8966,displayName:"FreshChoice -- Half Moon Bay'"}
+        # ]
 
     @classmethod
     def _get_random_jsonp_tag(cls):
@@ -84,7 +251,7 @@ class LasooCoNzDealSpider(scrapy.Spider):
 
     @classmethod
     def _get_retailer_detail_url(cls, catalogue_id, jsonp_tag):
-        return cls.RETAILER_INFO_URL_FORMAT.format(catalogue_id, jsonp_tag)
+        return cls.CATALOGUE_INFO_URL_FORMAT.format(catalogue_id, jsonp_tag)
 
     @classmethod
     def _get_deals_url(cls, catalogue_id, jsonp_tag):
@@ -108,125 +275,6 @@ class LasooCoNzDealSpider(scrapy.Spider):
         return {
             'Accept': '*/*',
             'Accept-Language': 'en-US,en;q=0.8',
-            'Host': cls.JSONP_HOST,
+            'Host': cls.REQUEST_HOST,
             'Referer': referer,
         }
-
-    def parse_retailer_details(self, response):
-        jsonp_tag = response.meta['jsonp_tag']
-        json_data = self.__class__._get_jsonp_response_data(response.body, jsonp_tag)
-        catalogues = json_data['catalogues']
-        if len(catalogues) == 0:
-            return []
-        catalogue = catalogues[0]
-        start_date = catalogue['startDate']
-        end_date = catalogue['expiryDate']
-        total_pages = catalogue['numberPages']
-        retailer_data = catalogue['retailer']
-        retailer = {
-            'name': retailer_data['name'],
-            'display_name': retailer_data['name'],
-            'logo_url': retailer_data['smallImage'],
-            'country': self.country,
-            'datasource': self.datasource,
-            'properties': [
-                {
-                    'name': make_internal_property_name('lasso_retailer_id'),
-                    'value': retailer_data['id'],
-                },
-                {
-                    'name': make_internal_property_name('lasso_unique_name'),
-                    'value': retailer_data['uniqueName'],
-                },
-                {
-                    'name': make_internal_property_name('lasso_code'),
-                    'value': retailer_data['code'],
-                }
-
-            ]
-        }
-
-        yield retailer_data
-        # Update jsonp for new request
-        jsonp_tag = self.__class__._get_random_jsonp_tag()
-        deal_url = self.__class__._get_deals_url(response.meta['catalogue_id'], jsonp_tag)
-        meta = response.meta
-        meta['start_date'] = start_date
-        meta['end_date'] = end_date
-        meta['retailer'] = retailer
-        meta['jsonp_tag'] = jsonp_tag
-        yield scrapy.Request(deal_url,
-                             callback=self.parse_deals,
-                             headers=self.__class__._get_http_headers(meta['referer']),
-                             meta=meta)
-
-    def parse_deals(self, response):
-        jsonp_tag = response.meta['jsonp_tag']
-        deals_data = self._get_jsonp_response_data(response.body, jsonp_tag)
-
-        meta = response.meta
-        # Update jsonp for new request
-        jsonp_tag = self.__class__._get_random_jsonp_tag()
-        meta['jsonp_tag'] = jsonp_tag
-
-
-        # if 'categoryPath' in offer:
-        #     cats = offer['categoryPath']
-        #     for c in cats:
-        #         category = SourceCategory()
-        #         c.name = c['uniqueName']
-        #         c.source_category_id = c['id']
-        #         c.display_name = c['name']
-        #         c.source = self.datasource
-        #         c.save()
-
-
-        for page in deals_data:
-            offers = page['offers']
-            if offers is None:
-                continue
-            for offer in offers:
-                if offer['type'] != 'offer':
-                    continue
-                deal = {}
-                deal['id'] = offer['id']
-                deal['title'] = offer['title']
-                if 'offerimage' in offer:
-                    deal['image'] = offer['offerimage']['path']
-                deal['landing_page'] = response.urljoin(offer['landingLink'])
-
-                meta['offer'] = deal
-                meta['referer'] = response.url
-                offer_json_url = self.__class__._get_offer_url(offer['id'], jsonp_tag)
-                yield scrapy.Request(offer_json_url,
-                                     callback=self.parse_offer_details,
-                                     headers=self.__class__._get_http_headers(meta['response.url']),
-                                     meta=meta)
-
-
-    def parse_offer_details(self, response):
-        meta = response.meta
-        # Parse Json
-        return scrapy.Request(meta['deal']['landing_page'],
-                              callback=self.parse_offer_stores,
-                              headers=self.__class__._get_http_headers(meta['referer']),
-                              meta=meta)
-
-    def parse_offer_stores(self, response):
-        # Parse store
-        script = extract_first_value_with_xpath(
-            '//section//div[@class="storemap-holder"]/following-sibling::script[1]/text()')
-
-        idx = script.find('showOfferDetailNearStoreMap')
-        if idx < 0:
-            return None
-        locations_json = substr_surrounded_by_chars(script, ('[', ']'), idx)
-        store_locations = json_loads(locations_json)
-        # [
-        #     {id:14540226467431,latitude:-36.9424592,longitude:174.7867304,displayName:"FreshChoice -- Mangere Bridge'"}
-        #     ,
-        #     {id:14776308161975,latitude:-36.9446156,longitude:174.8425279,displayName:"FreshChoice -- Otahuhu'"}
-        #     ,
-        #     {id:14540226467325,latitude:-36.8809,longitude:174.8966,displayName:"FreshChoice -- Half Moon Bay'"}
-        # ]
-        pass

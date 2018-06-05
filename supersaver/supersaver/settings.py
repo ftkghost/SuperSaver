64c14ce1467c589256f575d14e62815247abf4d3
pengqin <peng.qin@fiserv.com>
2018-06-05 17:53:59 +1200

    Update django settings to use postgreSQL engine by default.

--- supersaver/supersaver/settings.py
+++ supersaver/supersaver/settings.py
@@ -89,9 +89,17 @@ DATABASES = {
     #    'ENGINE': 'django.db.backends.sqlite3',
     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     #}
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'SuperSaver',
+    #     'USER': 'supersaver',
+    #     'PASSWORD': '',
+    #     'HOST': '',
+    #     'PORT': '',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'SuperSaver',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'supersaver',
         'USER': 'supersaver',
         'PASSWORD': '',
         'HOST': '',

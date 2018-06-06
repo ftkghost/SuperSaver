21e919867a99a80fe34ea8c6ae6c829f3a20220a
pengqin <peng.qin@fiserv.com>
2018-06-06 18:08:29 +1200

    Update setup DB script to activate extension for text search.

--- setup_postgresql_db.sh
+++ setup_postgresql_db.sh
@@ -6,8 +6,17 @@ db_host=localhost
 # * Install mysql
 brew install postgresql
 
+# Create a DB user with 'CREATEDB' attribute/permission.
 createuser ${db_user} --createdb
 
-psql -U ${db_user} <<EOF
-\password ${db_userpwd}
+# Set password for new user, you will be prompted to enter password
+psql postgres<<EOF
+\password ${db_user}
+EOF
+
+# Activate extensions for text search
+psql postgres<<EOF
+CREATE EXTENSION IF NOT EXISTS btree_gin;
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE EXTENSION IF NOT EXISTS unaccent;
 EOF
